785d3fa8ea721c84d7e3e081617ad695548d25b1
Purpursarkans <goga.danilov10@gmail.com>
2022-09-21 11:05:10 +0400

    update

--- SimpleGetCoord.cpp
+++ SimpleGetCoord.cpp
@@ -3,15 +3,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int MOVE_CONSOLE_X = 0;
+static int MOVE_CONSOLE_Y = 50;
+static int CONSOLE_WIDTH = 255;
+static int CONSOLE_HEIGHT = 120;
+
 int main(int argc, char* argv[])
 {
+
+    MoveWindow(GetConsoleWindow(), MOVE_CONSOLE_X, MOVE_CONSOLE_Y, CONSOLE_WIDTH, CONSOLE_HEIGHT, TRUE);
+    SetWindowPos(GetConsoleWindow(), HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+
     POINT P;
     int slp = 0;
 
-    //std::cout << "argc - " << argc << std::endl;
     for (int i = 0; i < argc; i++)
     {
-        //std::cout << "argv - " << "\"" << argv[i] << "\"" << std::endl;
         if(slp == 0)
         {
             slp = atoi(argv[i]);
@@ -21,10 +28,10 @@ int main(int argc, char* argv[])
     {
         GetCursorPos(&P);
         std::cout
+        << "----------------" << std::endl
         << "const int X = " << P.x << ";" << std::endl
         << "const int Y = " << P.y << ";" << std::endl
         << "----------------" << std::endl;
-        
         Beep(100, 100);
         if(slp == 0)
         {
